d31185c082f6b511b486f095fbf346f4a8d2ce7a
Toni Henriksson <henriksson.toni92@gmail.com>
2017-05-12 12:37:44 +0300

    Changes made lol, tells you much doesnt it :D

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,130 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        bool turn = true; //true = X turn; false = Y turn
+        int turn_count = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by: Toni Henriksson");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+            checkForWinner(); // AFTER EVERY BUTTON CLICK CHECK FOR WINNER METHOD RUNS
+        }
+
+        
+        private void checkForWinner()
+        {   // CHECKING HORIZONTAL SQUARES IF THERE IS A WINNER
+            
+            bool there_is_a_winner = false;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+
+
+            // VERTICAL CHECKS
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+
+            //CROSS CHECKS
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            if (there_is_a_winner)
+            {   // IF there_is_a_winner = true , MESSAGEBOX APPEARS AND BUTTONS GET DISABLED THIS WAY
+                disableButtons();
+                String winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+                MessageBox.Show(winner + " " + "Wins!" + " " + "Yay!");
+            }
+            else
+            {
+                if(turn_count == 9)
+                {
+                    MessageBox.Show("Draw!" ,  "Damn!");
+                }
+            }
+                
+        }
+
+        private void disableButtons()
+        {
+            //METHOD FOR DISABLING BUTTONS
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //METHOD FOR SETTING UP A NEW GAME 
+            turn = true;
+            turn_count = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+            catch { }
+
         }
     }
 }
